65a543476a60c38c032bbe1edbb1b4bcdc3d82d1
Yahor Babich <yahor.babich@gmail.com>
2020-05-23 00:47:59 -0400

    init

--- app/src/main/java/com/ruby/mvvm/view/list/ListActivity.kt
+++ app/src/main/java/com/ruby/mvvm/view/list/ListActivity.kt
@@ -2,6 +2,7 @@ package com.ruby.mvvm.view.list
 
 import android.os.Bundle
 import com.ruby.mvvm.R
+import com.ruby.mvvm.extension.observe
 import com.ruby.mvvm.model.data.DailyForecastModel
 import com.ruby.mvvm.util.ext.argument
 import com.ruby.mvvm.view.Arguments.ARG_ADDRESS
@@ -9,6 +10,8 @@ import com.ruby.mvvm.view.Arguments.ARG_WEATHER_DATE
 import com.ruby.mvvm.view.Arguments.ARG_WEATHER_ITEM_ID
 import com.ruby.mvvm.view.BaseActivity
 import com.ruby.mvvm.view.detail.DetailActivity
+import com.ruby.mvvm.view.list.model.ForwardModel
+import com.ruby.mvvm.view.list.model.ListModel
 import kotlinx.android.synthetic.main.activity_weather.*
 import org.jetbrains.anko.startActivity
 import org.koin.android.architecture.ext.viewModel
@@ -19,32 +22,16 @@ class ListActivity : BaseActivity() {
     private lateinit var weatherResultAdapter: ListAdapter
     val date: Date by argument(ARG_WEATHER_DATE)
 
-    val viewModel: ListViewModel by viewModel()
+    private val viewModel: ListViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather)
 
-        viewModel.uiData.observe(this, androidx.lifecycle.Observer { model ->
-            if (model != null) {
-                val weatherList = model.list
-                if (weatherList != weatherResultAdapter.list && weatherList.isNotEmpty()) {
-                    displayWeather(weatherList)
-                } else if (model.error != null) {
-                    displayError(model.error)
-                }
-            }
-        })
-
-        viewModel.selectEvent.observe(this, androidx.lifecycle.Observer { event ->
-            if (event != null) {
-                startActivity<DetailActivity>(
-                    ARG_ADDRESS to "address",
-                    ARG_WEATHER_DATE to Date(),
-                    ARG_WEATHER_ITEM_ID to event.id
-                )
-            }
-        })
+        viewModel.apply {
+            observe(uiData, ::display)
+            observe(selectEvent, ::next)
+        }
 
         weatherResultAdapter = ListAdapter(emptyList(), onItemClicked())
         weatherList.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(this)
@@ -53,13 +40,34 @@ class ListActivity : BaseActivity() {
         viewModel.getWeatherList()
     }
 
+    private fun display(model: ListModel?) {
+        model?.apply {
+            val weatherList = list
+            if (weatherList != weatherResultAdapter.list && weatherList.isNotEmpty()) {
+                displayWeather(weatherList)
+            } else if (error != null) {
+                displayError(error)
+            }
+        }
+    }
+
+    private fun next(model: ForwardModel?) {
+        model?.apply {
+            startActivity<DetailActivity>(
+                ARG_ADDRESS to "address",
+                ARG_WEATHER_DATE to Date(),
+                ARG_WEATHER_ITEM_ID to id
+            )
+        }
+    }
+
     private fun onItemClicked(): (DailyForecastModel) -> Unit {
         return { weatherDetail ->
             viewModel.selectWeatherDetail(weatherDetail.id)
         }
     }
 
-    fun displayWeather(weatherList: List<DailyForecastModel>) {
+    private fun displayWeather(weatherList: List<DailyForecastModel>) {
         weatherResultAdapter.list = weatherList
         weatherResultAdapter.notifyDataSetChanged()
     }
