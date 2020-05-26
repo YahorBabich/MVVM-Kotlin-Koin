887d0b7fa706fe608f58e85dcb3becafe3ed9694
Yahor Babich <yahor.babich@gmail.com>
2020-05-26 16:31:07 -0400

    coding

--- app/src/main/java/com/yahorb/mvvm/view/search/SearchActivity.kt
+++ app/src/main/java/com/yahorb/mvvm/view/search/SearchActivity.kt
@@ -22,7 +22,7 @@ class SearchActivity : BaseActivity() {
 
         search.setOnEditorActionListener { view, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                viewModel.search("jakson")
+                viewModel.search(view.text.trim().toString())
             }
             true
         }
